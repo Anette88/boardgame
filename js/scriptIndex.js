dfc16509e84a1f5bc875acc5c206163d98d848e2
Anette Gabrielsen <gabrielsen_anette@hotmail.com>
2021-01-24 20:23:54 +0100

    last commit

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,16 +1,3 @@
-// Data from JSON file to be displayed in cards
-
-//fetch('API/characters.json')
-	//.then(result => result.json())
-	//.then((res) => {
-		//retrieveData(res);
-	//})
-	//.catch(err => console.log(err))
-
-//function retrieveData(info) {
-
-//}
-
 
 function chooseCharacter1(character) {
 	if (character === "Tormund") {
